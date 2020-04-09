a1f62e58bd004a577ab6f6e0fffa414d8ecd681d
Jung Hong <junghong0512@gmail.com>
2020-04-09 15:17:45 +0900

    GET, ADD, FIND, UPDATE, DELETE methods are implemented on the article.js

--- routes/articles.js
+++ routes/articles.js
@@ -2,10 +2,52 @@ const express = require("express");
 const router = express.Router();
 const Articles = require("../models/articles");
 
-//Get the articles from the MongoDB
+// REQUEST GET ALL ARTICLES from the MongoDB
 router.get("/", (req, res) => {
   Articles.find()
     .then((article) => res.json(article))
+    .catch((err) => res.status(400).json(`Error: ${err}`));
+});
+
+// REQUEST ADD NEW ARTICLW
+router.post("/add", (req, res) => {
+  const newArticle = new Articles({
+    title: req.body.title,
+    article: req.body.article,
+    authorname: req.body.authorname,
+  });
+
+  newArticle
+    .save()
+    .then(() => res.json("The new Article posted successfully!"))
+    .catch((err) => res.status(400).json(`Error: ${err}`));
+});
+
+// REQUEST FIND ARTICLE BY ID
+router.get("/:id", (req, res) => {
+  Articles.findById(req.params.id)
+    .then((article) => res.json(article))
+    .catch((err) => res.status(400).json(`Error: ${err}`));
+});
+
+// REQUEST FIND ARTICLE BY ID AND UPDATE
+router.put("/update/:id", (req, res) => {
+  Articles.findById(req.params.id).then((article) => {
+    article.title = req.body.title;
+    article.article = req.body.article;
+    article.authorname = req.body.authorname;
+
+    article
+      .save()
+      .then(() => res.json("The Article is UPDATED successfully"))
+      .catch((err) => res.status(400).json(`Error: ${err}`));
+  });
+});
+
+// REQUEST FIND ARTICLE BY ID AND DELETE
+router.delete("/delete/:id", (req, res) => {
+  Articles.findByIdAndDelete(req.params.id)
+    .then(() => res.json("The Article is DELETED successfully"))
     .catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
